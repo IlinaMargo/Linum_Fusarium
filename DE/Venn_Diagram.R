a7891daa9f2c85edb0a3f4fd61811f690b5fabc4
milina <milina@aglab0.utrail.org>
2024-10-24 21:30:10 +0000

    add code Venn diagrams

--- DE/Venn_Diagram.R
+++ DE/Venn_Diagram.R
@@ -46,10 +46,27 @@ design(dds) <- ~ group
 # Проведение анализа
 dds <- DESeq(dds)
 
+
+
+###ИЗВЛЕЧЕНИЕ ГЕНОВ ИЗ КОНТРОЛЬНЫХ ОБРАЗЦОВ, ЧТОБЫ ВЫЧЕСТЬ ИЗ ОСТАЛЬНЫХ ХИТМАПОВ
+samples_AtK3_AtK5 <- colnames(dds)[dds$group %in% c("AtK3", "AtK5")]
+
+# Извлечение нормализованных данных для AtK3 и AtK5
+samples_AtK3_AtK5 <- colnames(dds)[dds$group %in% c("AtK3", "AtK5")]
+counts_AtK3_AtK5 <- counts(dds)[, samples_AtK3_AtK5]
+
+# Фильтрация генов НЕНОРМАЛИЗОВАННЫХ ДАННЫХ, у которых есть экспрессия хотя бы в одном образце (ненулевое значение)
+expressed_genes_AtK3_AtK5 <- rownames(counts_AtK3_AtK5[rowSums(counts_AtK3_AtK5 != 0) > 0, ])
+
+# Например, исключаем эти гены из объекта DESeq2
+dds <- dds[!rownames(dds) %in% expressed_genes_AtK3_AtK5, ]
+
+
+### Построение диаграмм Венна
 # Сравнение групп AtK3 и AtF5
-res_AtK3_AtF5 <- results(dds, contrast = c("group", "AtK3", "AtF5"))
-significant_genes_AtK3_AtF5 <- subset(res_AtK3_AtF5, padj <= 0.05 & abs(log2FoldChange) >= 1.5)
-expressed_genes_AtK3_AtF5 <- rownames(significant_genes_AtK3_AtF5[rowSums(counts(dds)[rownames(significant_genes_AtK3_AtF5), ] != 0) > 0, ])
+res_AtK3_AtF3 <- results(dds, contrast = c("group", "AtK3", "AtF3"))
+significant_genes_AtK3_AtF3 <- subset(res_AtK3_AtF3, padj <= 0.05 & abs(log2FoldChange) >= 1.5)
+expressed_genes_AtK3_AtF3 <- rownames(significant_genes_AtK3_AtF3[rowSums(counts(dds)[rownames(significant_genes_AtK3_AtF3), ] != 0) > 0, ])
 
 # Сравнение групп AtK5 и AtF5
 res_AtK5_AtF5 <- results(dds, contrast = c("group", "AtK5", "AtF5"))
@@ -61,23 +78,91 @@ res_AtF3_AtF5 <- results(dds, contrast = c("group", "AtF3", "AtF5"))
 significant_genes_AtF3_AtF5 <- subset(res_AtF3_AtF5, padj <= 0.05 & abs(log2FoldChange) >= 1.5)
 expressed_genes_AtF3_AtF5 <- rownames(significant_genes_AtF3_AtF5[rowSums(counts(dds)[rownames(significant_genes_AtF3_AtF5), ] != 0) > 0, ])
 
-# Сравнение групп AtK3 и AtK5
-res_AtK3_AtK5 <- results(dds, contrast = c("group", "AtK3", "AtK5"))
-significant_genes_AtK3_AtK5 <- subset(res_AtK3_AtK5, padj <= 0.05)
-expressed_genes_AtK3_AtK5 <- rownames(significant_genes_AtK3_AtK5[rowSums(counts(dds)[rownames(significant_genes_AtK3_AtK5), ] != 0) > 0, ])
-
 
 # список для диаграммы Вена
 gene_lists <- list(
-  "AtK3_vs_AtF5" = expressed_genes_AtK3_AtF5,
+  "AtK3_vs_AtF3" = expressed_genes_AtK3_AtF3,
   "AtK5_vs_AtF5" = expressed_genes_AtK5_AtF5,
-  "AtF3_vs_AtF5" = expressed_genes_AtF3_AtF5,
-  "AtK3_vs_AtK5" = expressed_genes_AtK3_AtK5
+  "AtF3_vs_AtF5" = expressed_genes_AtF3_AtF5
 )
 
-png("Venn_diagram_all.png", width = 12, height = 8, units = "in", res = 150, type="cairo")
+png("Venn_diagram_all.png", width = 10, height = 8, units = "in", res = 150, type="cairo")
 # Построение диаграммы Венна
 ggVennDiagram(gene_lists) +
-  scale_fill_gradient(low = "grey90", high = "red")  # Настройка цветовой шкалы
+  scale_fill_gradient(low = "grey90", high = "purple4") +
+  ggtitle("All different expressed genes") +
+  theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"))
 dev.off()
 
+
+
+
+### для up генов
+## AtF3 vs AtK3
+up_significant_genes_AtK3_AtF3 <- subset(res_AtK3_AtF3, padj <= 0.05 & log2FoldChange >= 1.5)
+up_expressed_genes_AtK3_AtF3 <- rownames(up_significant_genes_AtK3_AtF3[rowSums(counts(dds)[rownames(up_significant_genes_AtK3_AtF3), ] != 0) > 0, ])
+
+## AtF5 vs AtK5
+up_significant_genes_AtK5_AtF5 <- subset(res_AtK5_AtF5, padj <= 0.05 & log2FoldChange >= 1.5)
+up_expressed_genes_AtK5_AtF5 <- rownames(up_significant_genes_AtK5_AtF5[rowSums(counts(dds)[rownames(up_significant_genes_AtK5_AtF5), ] != 0) > 0, ])
+
+
+## AtF3 vs AtF5
+up_significant_genes_AtF3_AtF5 <- subset(res_AtF3_AtF5, padj <= 0.05 & log2FoldChange >= 1.5)
+up_expressed_genes_AtF3_AtF5 <- rownames(up_significant_genes_AtF3_AtF5[rowSums(counts(dds)[rownames(up_significant_genes_AtF3_AtF5), ] != 0) > 0, ])
+
+# список для диаграммы Вена
+gene_lists_up <- list(
+  "AtK3_vs_AtF3" = up_expressed_genes_AtK3_AtF3,
+  "AtK5_vs_AtF5" = up_expressed_genes_AtK5_AtF5,
+  "AtF3_vs_AtF5" = up_expressed_genes_AtF3_AtF5
+)
+
+png("Venn_diagram_up.png", width = 10, height = 8, units = "in", res = 150, type="cairo")
+# Построение диаграммы Венна
+ggVennDiagram(gene_lists_up) +
+  scale_fill_gradient(low = "grey90", high = "red") +
+  ggtitle("Up-regulated genes") +
+  theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"))
+dev.off()
+
+
+### для down генов
+## AtF3 vs AtK3
+down_significant_genes_AtK3_AtF3 <- subset(res_AtK3_AtF3, padj <= 0.05 & log2FoldChange <= - 1.5)
+down_expressed_genes_AtK3_AtF3 <- rownames(down_significant_genes_AtK3_AtF3[rowSums(counts(dds)[rownames(down_significant_genes_AtK3_AtF3), ] != 0) > 0, ])
+
+## AtF5 vs AtK5
+down_significant_genes_AtK5_AtF5 <- subset(res_AtK5_AtF5, padj <= 0.05 & log2FoldChange <= - 1.5)
+down_expressed_genes_AtK5_AtF5 <- rownames(down_significant_genes_AtK5_AtF5[rowSums(counts(dds)[rownames(down_significant_genes_AtK5_AtF5), ] != 0) > 0, ])
+
+## AtF3 vs AtF5
+down_significant_genes_AtF3_AtF5 <- subset(res_AtF3_AtF5, padj <= 0.05 & log2FoldChange <= - 1.5)
+down_expressed_genes_AtF3_AtF5 <- rownames(down_significant_genes_AtF3_AtF5[rowSums(counts(dds)[rownames(down_significant_genes_AtF3_AtF5), ] != 0) > 0, ])
+
+# список для диаграммы Вена
+gene_lists_down <- list(
+  "AtK3_vs_AtF3" = down_expressed_genes_AtK3_AtF3,
+  "AtK5_vs_AtF5" = down_expressed_genes_AtK5_AtF5,
+  "AtF3_vs_AtF5" = down_expressed_genes_AtF3_AtF5
+)
+
+png("Venn_diagram_down.png", width = 10, height = 8, units = "in", res = 150, type="cairo")
+# Построение диаграммы Венна
+ggVennDiagram(gene_lists_down) +
+  scale_fill_gradient(low = "grey90", high = "blue") +
+  ggtitle("Down-regulated genes") +
+  theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"))
+dev.off()
+
+
+
+# Проверка почему up+down не равно all:
+status_all_genes <- data.frame(
+  Gene = intersection_all,
+  AtK3_vs_AtF3 = ifelse(res_AtK3_AtF3[intersection_all, "log2FoldChange"] >= 1.5, "up", ifelse(res_AtK3_AtF3[intersection_all, "log2FoldChange"] <= -1.5, "down", "none")),
+  AtK5_vs_AtF5 = ifelse(res_AtK5_AtF5[intersection_all, "log2FoldChange"] >= 1.5, "up", ifelse(res_AtK5_AtF5[intersection_all, "log2FoldChange"] <= -1.5, "down", "none")),
+  AtF3_vs_AtF5 = ifelse(res_AtF3_AtF5[intersection_all, "log2FoldChange"] >= 1.5, "up", ifelse(res_AtF3_AtF5[intersection_all, "log2FoldChange"] <= -1.5, "down", "none"))
+)
+
+print(status_all_genes)
